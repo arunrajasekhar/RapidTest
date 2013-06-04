37c4c8f67814c2cc0b280960a413519467345ce7
arunrajasekhar <arunr@rapidvaluesolutions.com>
2013-06-04 14:28:55 +0530

    sample

---
+++ rapidTestMain/rapidTestMain/ATabularViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATabularViewController.h
+//  rapidTestMain
+//
+//  Created by Arun Raj on 17/05/13.
+//  Copyright (c) 2013 RapidValue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATabularViewController : UITableViewController
+
+@end
