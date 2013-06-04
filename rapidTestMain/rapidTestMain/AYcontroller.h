faf409a14e56accdab6bbba38fe939401345f0f8
arunrajasekhar <arunr@rapidvaluesolutions.com>
2013-06-04 14:41:21 +0530

    y committed

---
+++ rapidTestMain/rapidTestMain/AYcontroller.h
@@ -0,0 +1,13 @@
+//
+//  AYcontroller.h
+//  rapidTestMain
+//
+//  Created by Arun Raj on 04/06/13.
+//  Copyright (c) 2013 RapidValue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AYcontroller : NSObject
+
+@end
---
+++ rapidTestMain/rapidTestMain/AYcontroller.m
@@ -0,0 +1,13 @@
+//
+//  AYcontroller.m
+//  rapidTestMain
+//
+//  Created by Arun Raj on 04/06/13.
+//  Copyright (c) 2013 RapidValue. All rights reserved.
+//
+
+#import "AYcontroller.h"
+
+@implementation AYcontroller
+
+@end
